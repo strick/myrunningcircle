b9731dd27fe2e10b163d5843728e9bcf1bc96885
Brian <bstrick@gmail.com>
2021-04-07 20:34:31 -0400

    Using Express

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const http = require('http');
+const express = require('express');
+var app = express();
 
-var server = http.createServer(function (request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World\n");
+app.get('/', (req, res) => {
+    res.send('Hello Express')
 });
 
-server.listen(4000);
+app.listen(process.env.PORT || 3000)
