08c7e5e59a9ccd01ab53a6cdc4a4575bfa62c2fa
Brian <bstrick@gmail.com>
2021-04-07 21:25:38 -0400

    Dockerized

---
+++ web/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+var app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+console.log("hello");
+
+app.get('/', (req, res) => {
+    res.send('Hello Express')
+});
+
+app.listen(port, () => {
+    console.log(`Microservice listening on port ${port}, point your browser at http://localhost:3000/video`);
+});
