cdbe3d4cbd3df4ca438131dbfe4ec379dff69c94
Brian <bstrick@gmail.com>
2021-04-12 12:51:48 -0400

    Updated the ports and env docker-compos AB#244

--- web/controllers/feed-controller.js
+++ web/controllers/feed-controller.js
@@ -1,6 +1,12 @@
 module.exports=
 {
     feed:function(req, res) {
-        res.render('feed');
+        //res.render('feed');
+
+        // Forward the request to the feed-reader to get the content
+        conts forwardRequest = http.request {
+            
+        }
+
     }   
 }
