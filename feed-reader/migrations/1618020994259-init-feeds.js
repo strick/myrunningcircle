e46781941866432be265e487cb58a22d3f590ffd
Brian <bstrick@gmail.com>
2021-04-11 23:51:45 -0400

    Refactor didn't work... AB#239

--- feed-reader/migrations/1618020994259-init-feeds.js
+++ feed-reader/migrations/1618020994259-init-feeds.js
@@ -10,32 +10,53 @@ var MongoClient = require('mongodb').MongoClient;
 module.exports.up = next => {
  
   // make client connect to mongo service
-  return MongoClient.connect(url,
-    {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(client => {
-      
-      let db = client.db();
-
-      console.log("Switched to "+db.databaseName+" database");      
-      db.createCollection("running-feed");
-      
-      return client;
-
-    })
-    .then(client => {
-         
+  MongoClient.connect(url, function(err, client) {
+    const db = client.db();
+      if (err) throw err;
+      // db pointing to newdb
+      console.log("Switched to "+db.databaseName+" database");
+      // create 'users' collection in newdb database
+      db.createCollection("running-feed", function(err, result) {
+          if (err) throw err;
           console.log("Collection is created!");
+
+          let runs = [
+            { title:"My daily run", distance: "5" },
+            { title:"My weekly run", distance: "10" },
+            { title:"My weekly run", distance: "10" },
+            { title:"My weekly run", distance: "10" },
+            { title:"My monthly run", distance: "23" }
+          ];
+
+
+        //  console.log("test");
+          db.collection("running-feed").insertMany(runs);
+          // close the connection to db when you are done with it
           client.close();
-
-          // Gives:   warning : if your migration returns a promise, do not call the done callback
-          //return next();
-    })
-    .catch(err => {
-      console.error(err);
-    });
-
+          next();
+      });   
+  });;
 }
 
+module.exports.down = next => {
+ 
+  // make client connect to mongo service
+  MongoClient.connect(url, function(err, client) {
+    const db = client.db();
+      if (err) throw err;
+      // db pointing to newdb
+      console.log("Switched to "+db.databaseName+" database");
+      // create 'users' collection in newdb database
+      db.dropCollection("running-feed", function(err, result) {
+          if (err) throw err;
+          console.log("Collection is deleted!");
+          // close the connection to db when you are done with it
+          client.close();
+          next();
+      });   
+  });;
+}
+/*
 module.exports.down = next => {
 
   // make client connect to mongo service
@@ -63,3 +84,4 @@ module.exports.down = next => {
       console.error(err);
     });
 }
+*/
