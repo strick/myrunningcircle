458073174f5b250d9bde561066dbe72e371a95fd
Brian <bstrick@gmail.com>
2021-04-12 19:55:39 -0400

    Sending back mock data AB#257

--- strava-connector/controllers/index-controller.js
+++ strava-connector/controllers/index-controller.js
@@ -1,7 +1,14 @@
 module.exports=
 {
     index:function(req, res) {
-        res.send("HELLP");
+        
+        let runs = [
+            { title:"Strava: daily run", distance: "5" },
+            { title:"Strava: weekly run", distance: "10" },
+            { title:"Strava: monthly run", distance: "23" }
+          ];
+          res.setHeader('Content-Type', 'application/json');
+          res.send(JSON.stringify(runs));
 
     }   
 }
