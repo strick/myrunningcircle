203f31b194f045f3ca9f27147337c8c6a0574d2b
Brian <bstrick@gmail.com>
2021-04-13 19:45:14 -0400

    Moved strava redircto to .env AB#248

--- web/controllers/index-controller.js
+++ web/controllers/index-controller.js
@@ -5,8 +5,9 @@ module.exports=
     index:function(req, res) {
 
         const STRAVA_CLIENT_ID = process.env.STRAVA_CLIENT_ID;
+        const STRAVA_TOKEN_URL = process.env.STRAVA_TOKEN_URL;
 
-        res.redirect("https://www.strava.com/oauth/authorize?client_id=" + STRAVA_CLIENT_ID + "&response_type=code&redirect_uri=http://localhost:4000/auth/strava&approval_prompt=force&scope=activity:read_all");
+        res.redirect("https://www.strava.com/oauth/authorize?client_id=" + STRAVA_CLIENT_ID + "&response_type=code&redirect_uri=" + STRAVA_TOKEN_URL);
 
     },
 
