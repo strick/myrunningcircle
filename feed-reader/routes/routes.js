94da572acc1530669677a61d499880842415e2e2
Brian <bstrick@gmail.com>
2021-04-13 19:36:53 -0400

    cleanup AB#248

--- feed-reader/routes/routes.js
+++ feed-reader/routes/routes.js
@@ -1,6 +1,5 @@
 var express = require('express');
 const indexController = require('../controllers/index-controller')
-const feedController = require('../controllers/feed-controller');
 var router = express.Router();
 
 router.get('/', indexController.index );
