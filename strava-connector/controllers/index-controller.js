262efdbde45e037e9cfd973e70ad3647c941eb7b
Brian <bstrick@gmail.com>
2021-04-12 21:35:55 -0400

    Fixed to use dotenv AB#248

--- strava-connector/controllers/index-controller.js
+++ strava-connector/controllers/index-controller.js
@@ -2,7 +2,35 @@ module.exports=
 {
     index:function(req, res) {
         
+        //https://www.strava.com/api/v3/activities/{id}?include_all_efforts=" "Authorization: Bearer [[token]
 
+        console.log(process.env.STRAVA_TOKEN);
+/*
+        var StravaApiV3 = require('strava_api_v3');
+        var defaultClient = StravaApiV3.ApiClient.instance;
+
+        // Configure OAuth2 access token for authorization: strava_oauth
+        var strava_oauth = defaultClient.authentications['strava_oauth'];
+        strava_oauth.accessToken = "YOUR ACCESS TOKEN"
+
+        var api = new StravaApiV3.ActivitiesApi()
+
+        var opts = { 
+        'before': 56, // {Integer} An epoch timestamp to use for filtering activities that have taken place before a certain time.
+        'after': 56, // {Integer} An epoch timestamp to use for filtering activities that have taken place after a certain time.
+        'page': 56, // {Integer} Page number. Defaults to 1.
+        'perPage': 56 // {Integer} Number of items per page. Defaults to 30.
+        };
+
+        var callback = function(error, data, response) {
+        if (error) {
+            console.error(error);
+        } else {
+            console.log('API called successfully. Returned data: ' + data);
+        }
+        };
+        api.getLoggedInAthleteActivities(opts, callback);
+*/
         let runs = [
             { title:"Strava: daily run", distance: "5" },
             { title:"Strava: weekly run", distance: "10" },
