94c5e36b94fa5a5a3d73c869eb6e49f10027c4b5
Brian <bstrick@gmail.com>
2021-04-13 19:23:40 -0400

    Cleanup AB#248

--- feed-reader/controllers/index-controller.js
+++ feed-reader/controllers/index-controller.js
@@ -14,12 +14,7 @@ module.exports=
     get:function(req, res) {
         //res.render("Hello");
        api_helper.make_API_call("http://" + STRAVA_CONNECTOR_HOST + ":" + STRAVA_CONNECTOR_PORT + "/").then(response => {
-        res.json({runs: response});
-
-        console.log(response);
-       //  res.render("feed", {              
-        //    runs: response
-        // })
+            res.json({runs: response});
         })
         .catch(error => {
             res.send(error);
