7bb99b5ec82b4bf0a5d0bfa4f9db0e0763968ca6
Brian <bstrick@gmail.com>
2021-04-14 15:29:54 -0400

    refactor to promise AB#272

--- feed-reader/migrations/1618428093267-init-users.js
+++ feed-reader/migrations/1618428093267-init-users.js
@@ -1,9 +1,9 @@
 'use strict'
 
-module.exports.up = function (next) {
-  next()
+module.exports.up = next => {
+
 }
 
-module.exports.down = function (next) {
-  next()
+module.exports.down = next => {
+  
 }
