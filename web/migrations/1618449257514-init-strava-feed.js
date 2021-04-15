d82f5554e6f64bc92bb157252aebac7ee6b4f074
Brian <bstrick@gmail.com>
2021-04-15 10:09:23 -0400

    Restricting duplicate entries AB#277

--- web/migrations/1618449257514-init-strava-feed.js
+++ web/migrations/1618449257514-init-strava-feed.js
@@ -23,17 +23,21 @@ module.exports.up = next => {
       return db.createCollection("strava-feed")
       .then(collection => {
 
-        let runs = [
-          { title:"My daily run", distance: "5", fb_user_id: "10112072063160502", fb_user_name: "Brian Strickland", external_id: "1b6279d3-075f-4ac4-b426-e87899105532"},
-          { title:"My run", distance: "5", fb_user_id: "128332322", fb_user_name: "David Dupis", external_id: "82dad4ff-e99e-415d-b3b0-68d6b4d1e48c"},
-          { title:"My dddd run", distance: "5", fb_user_id: "10112072063160502", fb_user_name: "Brian Strickland", external_id: "7c092e2d-b0e9-453b-820b-70cd9f94313c" },
-          { title:"My 33 run", distance: "5", fb_user_id: "10112072063160502", fb_user_name: "Brian Strickland", external_id: "84b7604f-0436-4acc-bdc1-a26243cff123" },
-          { title:"My weekly run", distance: "10", fb_user_id: "128332322", fb_user_name: "David Dupis", external_id: "e167ac4c-2efb-43b3-8aa8-43c6b7c9af05" },
-          { title:"My monthly run", distance: "23", fb_user_id: "128332322", fb_user_name: "David Dupis", external_id: "21238c79-976b-4e1e-b3ce-a2c59764086d" }
-        ];
-
-        return db.collection("strava-feed").insertMany(runs);
+         return db.collection("strava-feed").createIndex({"external_id":1},{unique:true})
       })
+         .then(collection => {
+
+          let runs = [
+            { title:"My daily run", distance: "5", fb_user_id: "10112072063160502", fb_user_name: "Brian Strickland", external_id: "1b6279d3-075f-4ac4-b426-e87899105532"},
+            { title:"My run", distance: "5", fb_user_id: "128332322", fb_user_name: "David Dupis", external_id: "1b6279d3-075f-4ac4-b426-e87899105532"},
+            { title:"My dddd run", distance: "5", fb_user_id: "10112072063160502", fb_user_name: "Brian Strickland", external_id: "7c092e2d-b0e9-453b-820b-70cd9f94313c" },
+            { title:"My 33 run", distance: "5", fb_user_id: "10112072063160502", fb_user_name: "Brian Strickland", external_id: "84b7604f-0436-4acc-bdc1-a26243cff123" },
+            { title:"My weekly run", distance: "10", fb_user_id: "128332322", fb_user_name: "David Dupis", external_id: "e167ac4c-2efb-43b3-8aa8-43c6b7c9af05" },
+            { title:"My monthly run", distance: "23", fb_user_id: "128332322", fb_user_name: "David Dupis", external_id: "21238c79-976b-4e1e-b3ce-a2c59764086d" }
+          ];
+
+          return db.collection("strava-feed").insertMany(runs);
+        })
       .then(() => {
          
         console.log("Collection is created!");
