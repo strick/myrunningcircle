730f8d1400f063d5095e49fa15ada50f070900be
Brian <bstrick@gmail.com>
2021-04-27 13:35:43 -0400

    Added misssing vars AB#286

--- scripts/variables.tf
+++ scripts/variables.tf
@@ -5,3 +5,15 @@ variable "app_name" {
 variable "location" {
     default = "West US"
 }
+
+variable admin_username {
+  default = "linux_admin"
+}
+
+variable client_id {
+
+}
+
+variable client_secret {
+
+}
