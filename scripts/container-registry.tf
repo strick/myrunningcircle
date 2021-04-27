81827d0924d6d75f979dc5076225446ce02449b6
Brian <bstrick@gmail.com>
2021-04-27 13:00:19 -0400

    Updated container-registry.tf to use vars AB#286

--- scripts/container-registry.tf
+++ scripts/container-registry.tf
@@ -1,9 +1,9 @@
 resource "azurerm_container_registry" "container_registry" {
   
-    name = "myrunningcircle"
+    name = var.app_name
     resource_group_name = azurerm_resource_group.myrunningcircle.name
 
-    location = "eastus"
+    location = var.location
     admin_enabled = true
     sku = "Basic"
 
