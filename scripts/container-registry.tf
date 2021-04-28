9547285ecef747afdc4d1ba77aa2196ab256b43e
Brian <bstrick@gmail.com>
2021-04-28 18:59:13 -0400

    Deploying to kubes!  with error AB#286

--- scripts/container-registry.tf
+++ scripts/container-registry.tf
@@ -8,3 +8,16 @@ resource "azurerm_container_registry" "container_registry" {
     sku = "Basic"
 
 }
+
+output "registry_hostname" {
+  value = azurerm_container_registry.container_registry.login_server
+}
+
+output "registry_un" {
+  value = azurerm_container_registry.container_registry.admin_username
+}
+
+output "registry_pw" {
+  value = azurerm_container_registry.container_registry.admin_password
+  sensitive = true
+}
