b5f747c312881e79b451d46b2f6036f322d691d0
Brian <bstrick@gmail.com>
2021-04-28 23:04:24 -0400

    config files

---
+++ web/configs/config.js
@@ -0,0 +1,17 @@
+'use strict'
+
+if(process.env.MONGODB_HOST == undefined){
+    const dotenv = require('dotenv');
+    dotenv.config(); 
+}
+
+module.exports = {
+
+    dbhost: process.env.DBHOST,
+
+    dbname: process.env.DBNAME,
+
+    feedTable: "runnning-feed",
+
+    usersTable: "users"
+}
