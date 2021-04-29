cb36be64dd2ff763011af55d9cc946200808ae6b
Brian <bstrick@gmail.com>
2021-04-29 10:55:10 -0400

    IP changed and went back to old kube plan AB#286

--- scripts/kubernetes-cluster.tf
+++ scripts/kubernetes-cluster.tf
@@ -18,7 +18,7 @@ resource "azurerm_kubernetes_cluster" "cluster" {
     default_node_pool {
         name            = "default"
         node_count      = 1
-        vm_size         = "Standard_B1s"
+        vm_size         = "Standard_B2s"
     }
 
     service_principal {
